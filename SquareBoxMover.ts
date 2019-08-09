3598792e633ab305bff8a3996add9d54d293ad4a
Anwesh43 <anweshthecool0@gmail.com>
2019-08-09 21:15:11 +0530

    adding DrawingUtil class

--- SquareBoxMover.ts
+++ SquareBoxMover.ts
@@ -53,3 +53,30 @@ class ScaleUtil {
         return scale + scGap * dir
     }
 }
+
+class DrawingUtil {
+
+    static drawSBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const size : number = Math.min(w, h) / rectSizeFactor
+        const boxSize : number = Math.min(w, h) / boxSizeFactor
+        var deg : number = 0
+        var x : number = 0
+        for (var i = 0; i < parts; i++) {
+            const sc : number = ScaleUtil.divideScale(scale, i, parts)
+            x = (size) * sc
+            deg += Math.PI/2 * Math.floor(sc)
+        }
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = foreColor
+        context.fillStyle = foreColor
+        context.strokeRect(-size / 2, -size / 2, size, size)
+
+        context.save()
+        context.rotate(deg)
+        context.fillRect(x, -boxSize, boxSize, boxSize)
+        context.restore()
+    }
+}
