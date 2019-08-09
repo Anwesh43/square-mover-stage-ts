4d8c0baa9fd19b75e8d71b61e7242d598b75eb34
Anwesh43 <anweshthecool0@gmail.com>
2019-08-09 20:47:50 +0530

    adding ScaleUtil class

--- SquareBoxMover.ts
+++ SquareBoxMover.ts
@@ -38,3 +38,18 @@ class SquareBoxMoverStage {
         stage.handleTap()
     }
 }
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+
+    static updateValue(scale : number, dir : number) : number {
+        return scale + scGap * dir
+    }
+}
