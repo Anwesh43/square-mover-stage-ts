522fbb5ff22e71224a9d3de645e01f5b8c70d5ea
Anwesh43 <anweshthecool0@gmail.com>
2019-08-09 21:27:50 +0530

    using renderer in stage

--- SquareBoxMover.ts
+++ SquareBoxMover.ts
@@ -12,6 +12,7 @@ class SquareBoxMoverStage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w
@@ -23,11 +24,14 @@ class SquareBoxMoverStage {
     render() {
         this.context.fillStyle = backColor
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
